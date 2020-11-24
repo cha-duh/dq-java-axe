4b0b238a5b2ae4ad4348863fc485ad00f2129990
Joshua Owens <jowens@palantir.com>
2020-11-24 14:36:54 -0500

    more closely aligned to example code, bumped module settings to 1.8 compatibility for lambda

--- src/test/java/JavaAxeTests.java
+++ src/test/java/JavaAxeTests.java
@@ -1,9 +1,6 @@
 import env.EnvManager;
 import env.RunEnvironment;
 
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 
 import org.junit.Assert;
@@ -13,6 +10,8 @@ import org.junit.Test;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.WebElement;
 
 import com.deque.html.axecore.selenium.AxeBuilder;
 import com.deque.html.axecore.axeargs.AxeRunOptions;
@@ -21,9 +20,19 @@ import com.deque.html.axecore.results.Rule;
 
 public class JavaAxeTests {
 
+    private WebDriver webDriver;
+    private WebDriverWait wait;
+
     @Before
     public void startBrowser() {
+
+        // update this string for alternate tests
+        // String testURL = "localhost:5005"
+        String testURL = "https://dequeuniversity.com/demo/mars/";
         EnvManager.initWebDriver();
+        this.webDriver = RunEnvironment.getWebDriver();
+        this.webDriver.get(testURL);
+        this.wait.until(drv -> drv.findElement(By.cssSelector("main")));
     }
 
     @Deprecated
@@ -35,42 +44,34 @@ public class JavaAxeTests {
     }
 
     @Test
+    public void mainElementLoaded() {
+        WebElement mainElement = wait.until(drv -> drv.findElement(By.cssSelector("main")));
+        AxeBuilder builder = new AxeBuilder();
+        Results results = builder.analyze(this.webDriver, mainElement);
+        Assert.assertNotNull(results);
+    }
+
+    @Test
     public void axe() {
-        long timeBeforeScan = new Date().getTime();
 
-        WebDriver webDriver = RunEnvironment.getWebDriver();
-        webDriver.get("https://dequeuniversity.com/demo/mars/");
-
-
+        //this.webDriver.get(testURL);
 
         AxeRunOptions runOptions = new AxeRunOptions();
         runOptions.setXPath(true);
 
-        AxeBuilder builder = new AxeBuilder().withOptions(runOptions)
-                .withTags(Arrays.asList("wcag2a", "wcag412"));
+        AxeBuilder builder = new AxeBuilder().withOptions(runOptions);
+                //.withTags(Arrays.asList("wcag2a", "wcag412"));
 
+        // run analyze and pull out violations
         Results results = builder.analyze(webDriver);
-
         List<Rule> violations = results.getViolations();
-        List<Rule> passes = results.getPasses();
+        int vSize = violations.size();
 
-        printUtil(violations);
-        printUtil(passes);
+        // send violations to 'console' per guidelines if there are any
+        if (vSize > 0) { printUtil("Violations", violations); }
 
-        // these are straight up stolen from the example axe integration
-        // i don't have any expectations from the demo site, so no assertions to be made
-        Assert.assertNotNull(violations.get(0).getId());
-        Assert.assertNotEquals("color-contrast", violations.get(0).getId());
-        Assert.assertNotNull(results.getViolations().get(0).getTags());
-        Assert.assertTrue(results.getViolations().get(0).getTags().contains("wcag2a"));
-        Assert.assertTrue(results.getViolations().get(0).getTags().contains("wcag412"));
-        Assert.assertEquals(3, violations.size());
-        Assert.assertNotNull(results.getViolations().get(0).getNodes());
-
-        long time = new Date().getTime();
-        Assert.assertNotEquals(time, timeBeforeScan);
-        Assert.assertTrue(time > timeBeforeScan);
-        System.out.print(time - timeBeforeScan);
+        // fail test if there are accessibility violations
+        Assert.assertEquals(0, vSize);
     }
 
     @After
@@ -78,12 +79,27 @@ public class JavaAxeTests {
         EnvManager.shutDownDriver();
     }
 
-    private void printUtil(List<Rule> items) {
+    private void printUtil(String type, List<Rule> items) {
+        String summary = "\nPrinting summary of: ";
+        String rawData = "\nPrinting raw data from: ";
+
+        // quick header for type of output
+        System.out.print(summary);
+        System.out.print(type);
+        System.out.println();
+
+        // more legible rules
         for (Rule item : items) {
             System.out.println(item.getId());
             System.out.println(item.getHelp());
             System.out.println(item.getHelpUrl());
             System.out.println();
         }
+
+        // dump raw data with header
+        System.out.print(rawData);
+        System.out.print(type);
+        System.out.println();
+        System.out.println(items.toString());
     }
 }
